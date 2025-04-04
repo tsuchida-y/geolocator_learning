64149b690502402ddafa55b562924bcccba935c6
tsuchida <yu41926t.tutida@icloud.com>
2025-04-05 04:45:55 +0900

    現在の経度緯度を表示するように変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// 位置情報の権限を確認する関数
+Future<void> checkPermission() async {
+  LocationPermission permission = await Geolocator.checkPermission();
+  
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      print("位置情報のアクセスが拒否されました。");
+      return;
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    print("設定から位置情報の権限を有効にしてください。");
+    return;
+  }
+}
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,13 +52,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // 現在地を格納する変数
+  double? _latitude;
+  double? _longitude;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  //現在の位置を取得する関数
+Future<void> getCurrentLocation() async {
+  await checkPermission(); // 権限を確認
+  try {
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(accuracy: LocationAccuracy.high),
+    );    
+    print("現在地: 緯度 ${position.latitude}, 経度 ${position.longitude}");
+          // 現在地を更新して画面を再描画
+      setState(() {
+        _latitude = position.latitude;
+        _longitude = position.longitude;
+      });
+  } catch (e) {
+    print("位置情報の取得に失敗しました: $e");
   }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -50,18 +86,25 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '現在地:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // 緯度と経度を表示
+            if (_latitude != null && _longitude != null)
+              Text(
+                '緯度: $_latitude, 経度: $_longitude',
+                style: const TextStyle(fontSize: 16),
+              )
+            else
+              const Text(
+                '位置情報が取得されていません。',
+                style: TextStyle(fontSize: 16),
+              ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: getCurrentLocation,
+        tooltip: 'Get Location',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
